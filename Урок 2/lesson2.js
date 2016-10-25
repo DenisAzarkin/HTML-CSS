e201bd8403a034389e5f14e9d59b00924655831c
DenisAzarkin <azarkin.denis@gmail.com>
2016-10-25 19:49:03 +0300

    Исправлен код 3-й задачи. Теперь он выдаёт корректный ответ

--- Урок 2/lesson2.js
+++ Урок 2/lesson2.js
@@ -3,13 +3,18 @@
 var i; 
 
 for (i = 1; i <= 100; ++i) {
-	if ((i % 3) == 0) {
-		document.writeln("Fizz=", + i);
+	
+	if (((i % 5) != 0) && ((i % 3) != 0) ) {
+		document.writeln(i + "<br>");
 	}
-	if ((i % 5) == 0) {
-		document.writeln("Buzz=", + i);
+	else if (((i % 5) == 0) && ((i % 3) == 0) ) {
+		document.writeln("FizzBuzz" + "<br>");
 	}
-	if (((i % 5) == 0) && ((i % 3) == 0) ) {
-		document.writeln("FizzBuzz=", + i);
+	else if ((i % 3) == 0) {
+		document.writeln("Fuzz" + "<br>");
 	}
+	else if (((i % 5) == 0)) {
+		document.writeln("Buzz" + "<br>");
+	}
+	
 }
