cfb0dac065fade67906cd26d36157b957a664fd6
DenisAzarkin <azarkin.denis@gmail.com>
2016-10-28 16:37:07 +0300

    Лишний пробел убрал

--- Урок 2/lesson2.js
+++ Урок 2/lesson2.js
@@ -4,16 +4,16 @@ var i;
 
 for (i = 1; i <= 100; ++i) {
 	
-	if (((i % 5) != 0) && ((i % 3) != 0) ) {
+	if (((i % 5) != 0) && ((i % 3) != 0 )) {
 		document.writeln(i + "<br>");
 	}
-	else if (((i % 5) == 0) && ((i % 3) == 0) ) {
+	else if (((i % 5) == 0) && ((i % 3) == 0)) {
 		document.writeln("FizzBuzz" + "<br>");
 	}
 	else if ((i % 3) == 0) {
 		document.writeln("Fuzz" + "<br>");
 	}
-	else if (((i % 5) == 0)) {
+	else if ((i % 5) == 0) {
 		document.writeln("Buzz" + "<br>");
 	}
 	
